fbf2ebb239e915a6c1ce894ebac8e46846e3d365
Alexandre1832686 <1832686@etu.cchic.ca>
2023-04-13 11:00:22 -0400

    alex

---
+++ devappProjet2/devappProjet2/model/calcul.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace devappProjet2.model
+{
+    internal class calcul
+    {
+        float totalCapital;
+        float tauxInteret;
+        int periode;
+        string? frequence;
+
+        public calcul() { }
+
+        public calcul(float totalCapital, float tauxInteret, int periode, string? frequence)
+        {
+            this.totalCapital = totalCapital;
+            this.tauxInteret = tauxInteret;
+            this.periode = periode;
+            this.frequence = frequence;
+        }
+    }
+}
---
+++ devappProjet2/devappProjet2/vue/voir.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace devappProjet2.vue
+{
+    /// <summary>
+    /// Logique d'interaction pour voir.xaml
+    /// </summary>
+    public partial class voir : Window
+    {
+        public voir()
+        {
+            InitializeComponent();
+        }
+    }
+}
